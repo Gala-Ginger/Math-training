26aaf16304223774cf64b92d31bf5bcf0b4f6a49
Galina Znakharchuk <gala.znaharchuk@gmail.com>
2019-03-29 13:57:59 +0300

    init repa

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,163 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="training">
+    <hi>Math training. Level {{ level + 1 }}</hi>
+    <hr />
+    <div class="progress">
+      <div class="progress-bar" :style="progressStyles"></div>
+    </div>
+    <div class="box">
+      <transition name="flip" mode="out-in">
+        <app-start-screen v-if="state == 'start'" @onStart="onStart">
+        </app-start-screen>
+        <app-question
+          v-else-if="state == 'question'"
+          @success="onQuestSuccess"
+          @error="onQuestError"
+          :settings="levels[level]"
+        >
+        </app-question>
+        <app-message
+          v-else-if="state == 'message'"
+          :type="message.type"
+          :text="message.text"
+          @onNext="onNext"
+        >
+        </app-message>
+        <app-result-screen
+          v-else-if="state == 'result'"
+          :stats="stats"
+          @repeat="onStart"
+          @nextLevel="onNextLevel"
+        ></app-result-screen>
+        <div v-else>Unknown state</div>
+      </transition>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      state: "start",
+      stats: {
+        success: 0,
+        error: 0
+      },
+      message: {
+        type: "",
+        text: ""
+      },
+      questMax: 3,
+      level: 0,
+      levels: [
+        {
+          from: 10,
+          to: 40,
+          range: 5,
+          variants: 2
+        },
+        {
+          from: 100,
+          to: 200,
+          range: 20,
+          variants: 4
+        },
+        {
+          from: 500,
+          to: 900,
+          range: 40,
+          variants: 6
+        }
+      ]
+    };
+  },
+  computed: {
+    questDone() {
+      return this.stats.success + this.stats.error;
+    },
+    progressStyles() {
+      return {
+        width: (this.questDone / this.questMax) * 100 + "%"
+      };
+    }
+  },
+  methods: {
+    onStart() {
+      this.state = "question";
+      this.stats.success = 0;
+      this.stats.error = 0;
+    },
+    onQuestSuccess() {
+      this.state = "message";
+      this.message.text = "Good job";
+      this.message.type = "success";
+      this.stats.success++;
+    },
+    onQuestError(msg) {
+      this.state = "message";
+      this.message.text = msg;
+      this.message.type = "warning";
+      this.stats.error++;
+    },
+    onNext() {
+      if (this.questDone < this.questMax) {
+        this.state = "question";
+      } else {
+        this.state = "result";
+      }
+    },
+    onNextLevel() {
+      this.level++;
+      this.onStart();
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.training {
+  max-width: 700px;
+  margin: 20px auto;
+}
+
+.box {
+  margin: 10px 0;
+}
+
+.progress-bar {
+  transition: width 0.5s;
+}
+
+.flip-enter {
+}
+
+.flip-enter-active {
+  animation: flipInY 0.5s linear;
+}
+
+.flip-leave {
+}
+
+.flip-leave-active {
+  animation: flipOutY 0.5s linear;
+}
+
+@keyframes flipInY {
+  from {
+    transform: rotate(360deg);
+  }
+  to {
+    transform: rotate(0deg);
+  }
+}
+
+@keyframes flipOutY {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import AppMessage from './components/Message.vue';
+import AppQuestion from './components/Question.vue';
+import AppStartScreen from './components/StartScreen.vue';
+import AppResultScreen from './components/ResultScreen.vue';
+
+Vue.component('AppMessage', AppMessage);
+Vue.component('AppQuestion', AppQuestion);
+Vue.component('AppStartScreen', AppStartScreen);
+Vue.component('AppResultScreen', AppResultScreen);
+
 Vue.config.productionTip = false
 
 new Vue({
